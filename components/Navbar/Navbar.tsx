08ee25fdd94d9b10b0b92e538ac45f7133e03789
rl16432 <rluo52237@gmail.com>
2023-02-06 15:17:56 +1300

    fix: center logo on small screens

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -114,27 +114,27 @@ const Navbar = (): JSX.Element => {
               </Link>
             </NavbarMenu>
           </Box>
-          <CatchingPokemonIcon
-            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
-          />
-          <Link href="/" passHref>
-            <Typography
-              variant="h5"
-              noWrap
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".1rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              PokeTeam
-            </Typography>
-          </Link>
+          <Box sx={{flexGrow: 1, flexDirection: "row", alignItems: "center", display: { xs: "flex", md: "none" }}}>
+            <CatchingPokemonIcon
+              sx={{ mr: 1 }}
+            />
+            <Link href="/" passHref>
+              <Typography
+                variant="h5"
+                noWrap
+                sx={{
+                  mr: 2,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".1rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                PokeTeam
+              </Typography>
+            </Link>
+          </Box>
           <Box
             sx={{
               flexGrow: 1,
